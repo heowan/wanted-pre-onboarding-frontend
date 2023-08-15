b7a05af27e003fe0789760e23aacfdd707dc2048
heowan <101536766+heowan@users.noreply.github.com>
2023-08-15 16:35:16 +0900

    feat: 회원가입 페이지 레이아웃 구현

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <>
-      <div>회원가입</div>
-    </>
+    <div>
+      <input
+        data-testid="email-input"
+        type="email"
+        onChange={handleChangeEmail}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        onChange={handleChangePassword}
+      />
+      <button data-testid="signup-button">회원가입</button>
+    </div>
   );
 };
 
